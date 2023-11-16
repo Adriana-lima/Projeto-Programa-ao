acaf79314410b9e4bdf1e7a7120fd9e359fc3bc0
Adriana Costa Monteiro de Lima <141766086+Adriana-lima@users.noreply.github.com>
2023-11-16 14:22:22 -0300

    adicionei algumas variaveis

---
+++ projeto.py
@@ -0,0 +1,11 @@
+#inicio de um sonho
+mesada=int(input("digite sua mesada/renda total:"))
+alimentacao=int(input("digite seu gasto de alimentacao"))
+transporte=int(input("digite seu gasto de transporte"))
+saude=int(input("digite seu gasto de saude"))
+pessoal=int(input("digite seu gasto pessoal"))
+pet=int(input("digite seu gasto com seu pet"))
+faculdade=int(input("digite seu gasto com faculdade"))
+dividas=int(input("digite seu gasto com dividas"))
+def main():
+
